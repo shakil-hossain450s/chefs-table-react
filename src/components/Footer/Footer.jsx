8166f1c82b31b66aa6b935af80ab4a381f31d46e
shakil <shakil.hossain450s@gmail.com>
2024-10-17 17:04:31 +0600

    json image url set

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,9 +1,9 @@
 const Footer = () => {
     return (
         <>
-            <footer className="footer footer-center bg-base-300 text-base-content p-4">
+            <footer className="footer footer-center bg-base-200 text-base-content p-4">
                 <aside>
-                    <p>Copyright © {new Date().getFullYear()} - All right reserved by ACME Industries Ltd</p>
+                    <p>Copyright © 2024 - All right reserved by <span className="font-semibold text-lg">Chefs Table</span></p>
                 </aside>
             </footer>
         </>
--- src/components/Main/Recipes/Recipes.jsx
+++ src/components/Main/Recipes/Recipes.jsx
@@ -44,7 +44,7 @@ const Recipes = () => {
     return (
         <div>
             <SectionHeading></SectionHeading>
-            <div className='grid grid-cols-5 gap-6'>
+            <div className='grid grid-cols-1 lg:grid-cols-5 gap-6'>
                 <div className='col-span-3 grid grid-cols-1 md:grid-cols-2 gap-6'>
                     {
                         recipes.map(recipe => <Recipe
