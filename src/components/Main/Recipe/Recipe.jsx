b0312576cf49553adf872503aa1ab56975453688
shakil <shakil.hossain450s@gmail.com>
2024-10-17 14:18:36 +0600

    single recipe show in ui

---
+++ src/components/Main/Recipe/Recipe.jsx
@@ -0,0 +1,47 @@
+import PropTypes from 'prop-types';
+import { AiOutlineFire } from 'react-icons/ai';
+import { IoTimeOutline } from 'react-icons/io5';
+
+const Recipe = ({ recipe }) => {
+    const { recipe_image, recipe_name, short_description: recipe_description, ingredients, preparing_time, calories } = recipe;
+    return (
+        <div className='card border border-[#28282833] p-4 rounded-2xl'>
+            <figure className='rounded-2xl h-48 mb-3 lg:mb-6'>
+                <img
+                    src={recipe_image}
+                    alt={`recipe image of ${recipe_name}`} />
+            </figure>
+            <div className="card-body p-0">
+                <h2 className="card-title text-xl font-semibold text-[#282828] mb-4">{recipe_name}</h2>
+                <p>{recipe_description}</p>
+                <div className='mt-4'>
+                    <h2>Ingredients: <span className='font-semibold'>{ingredients.length}</span></h2>
+                    <ul className='ml-8 mt-2'>
+                        {
+                            ingredients.map((ingredient, idx) => <li key={idx} className='list-disc text-sm text-[#878787]'>{ingredient}</li>)
+                        }
+                    </ul>
+                </div>
+                <div className='py-6 flex items-center gap-4 text-[#282828CC]'>
+                    <p className='flex items-center gap-1'>
+                        <IoTimeOutline className='text-2xl'></IoTimeOutline>
+                        {preparing_time} minutes</p>
+                    <p className='flex items-center gap-1'>
+                        <AiOutlineFire className='text-2xl'></AiOutlineFire>
+                        {calories} calories</p>
+                </div>
+                <div className="card-actions">
+                    <button className="btn text-[#150B2B] bg-[#0BE58A] hover:bg-[#0BE58A] rounded-full border-0 px-6">
+                        Want to Cook
+                    </button>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+Recipe.propTypes = {
+    recipe: PropTypes.object.isRequired
+}
+
+export default Recipe;
--- src/components/Main/Recipes/Recipes.jsx
+++ src/components/Main/Recipes/Recipes.jsx
@@ -1,10 +1,30 @@
-import React from 'react';
+import { useEffect, useState } from 'react';
 import SectionHeading from '../../SectionHeading/SectionHeading';
+import Recipe from '../Recipe/Recipe';
 
 const Recipes = () => {
+    const [recipes, setRecipes] = useState([]);
+    useEffect(() => {
+        fetch("./recipes.json")
+            .then(res => res.json())
+            .then(data => setRecipes(data))
+    }, [])
     return (
         <div>
             <SectionHeading></SectionHeading>
+            <div className='grid grid-cols-3 gap-6'>
+                <div className='col-span-2 grid grid-cols-1 md:grid-cols-2 gap-6'>
+                    {
+                        recipes.map(recipe => <Recipe
+                            key={recipe.id}
+                            recipe={recipe}
+                        ></Recipe>)
+                    }
+                </div>
+                <div className='col-span-1 border'>
+                    <h2>Cooking Info</h2>
+                </div>
+            </div>
         </div>
     );
 };
